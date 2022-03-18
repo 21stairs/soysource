151a09e5e93dae89c627c283f25b91f5ee9bd9f4
qnfzks111 <qnfzks111@naver.com>
2022-03-18 10:25:40 +0900

    필요한 라이브러리 및 뼈대 미완성

---
+++ FE/soySouceFactoryManager/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Counter } from "./features/counter/Counter";
+import "./App.scss";
+// import { Router } from "react-router-dom";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;
