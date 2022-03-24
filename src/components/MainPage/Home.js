9668c465b2a32b0475eb531f4d84d03560455c54
qnfzks111 <qnfzks111@naver.com>
2022-03-24 09:57:09 +0900

    메인페이지와 WebRTC 기능 분리

---
+++ src/components/MainPage/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const test = () => {
+  return (
+    <div>
+      <p>메인 페이지</p>
+    </div>
+  );
+};
+
+export default test;
--- src/server/firebase.js
+++ src/server/firebase.js
@@ -2,9 +2,8 @@ import firebase from "firebase";
 
 var firebaseConfig = {
   apiKey: "AIzaSyAeOZNkWf-ssQHySRTw0JsZcZbBEueYknY", // Add API Key
-  databaseURL:"https://webrtc-f81d9-default-rtdb.firebaseio.com" // Add databaseURL
+  databaseURL: "https://webrtc-f81d9-default-rtdb.firebaseio.com", // Add databaseURL
 };
-
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -13,7 +12,7 @@ export const db = firebase;
 
 var firepadRef = firebase.database().ref();
 
-export const userName = prompt("What's your name?");
+export const userName = "zz";
 const urlparams = new URLSearchParams(window.location.search);
 const roomId = urlparams.get("id");
 
@@ -21,7 +20,6 @@ if (roomId) {
   firepadRef = firepadRef.child(roomId);
 } else {
   firepadRef = firepadRef.push();
-  window.history.replaceState(null, "Meet", "?id=" + firepadRef.key);
 }
 
 export default firepadRef;
